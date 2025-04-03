49fa7ee605ba7115aec30a3b2a49ea9762628883
Maxfratq <maximdocenko95@gmail.com>
2025-04-03 20:49:21 +0200

    Update and rename projekt_3.py to main.py

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 """
-projekt_3.py: třetí projekt do Engeto Online Python Akademie
+main.py: třetí projekt do Engeto Online Python Akademie
 
 author: Maxim Dotsenko 
 email: maximdocenko95@gmail.com
@@ -11,28 +11,29 @@ import csv
 import requests
 from bs4 import BeautifulSoup
 
-
+# Globální seznamy pro uchování dat
 volici = []
 obalky = []
 platne_hlasy = []
 
 def nacti_stranku(url):
-    
+    """Stáhne HTML z dané URL a vrátí objekt BeautifulSoup."""
     odpoved = requests.get(url)
     stranka = BeautifulSoup(odpoved.text, "html.parser")
     print("STAHUJI DATA Z URL:", url)
     return stranka
 
-
-if len(sys.argv) == 2:
-    region_stranka = nacti_stranku(sys.argv[1])
-    vystupni_soubor = "volby.csv"  # Výstupní soubor bude volby.csv
+# Skript očekává přesně 2 argumenty: URL a název výstupního CSV souboru.
+if len(sys.argv) == 3:
+    region_url = sys.argv[1]
+    vystupni_soubor = sys.argv[2]
+    region_stranka = nacti_stranku(region_url)
 else:
-    print('Chyba: Zadejte přesně jeden argument (URL adresu). Použijte: python projekt_3.py "URL_adresa"')
+    print('Chyba: Zadejte přesně dva argumenty: URL adresu a název výstupního CSV souboru.')
     sys.exit(1)
 
 def ziskej_nazvy_obci():
-    
+    """Vrací seznam názvů obcí z regionové stránky."""
     nazvy = []
     bunky = region_stranka.find_all("td", class_="overflow_name")
     for bunka in bunky:
@@ -40,7 +41,7 @@ def ziskej_nazvy_obci():
     return nazvy
 
 def ziskej_detailni_url():
-   
+    """Vrací seznam URL adres detailních stránek pro jednotlivé obce."""
     url_list = []
     bunky = region_stranka.find_all("td", class_="cislo", href=True)
     for bunka in bunky:
@@ -50,7 +51,7 @@ def ziskej_detailni_url():
     return url_list
 
 def ziskej_id_obci():
-   
+    """Vrací seznam identifikačních čísel obcí."""
     id_list = []
     bunky = region_stranka.find_all("td", class_="cislo")
     for bunka in bunky:
@@ -58,9 +59,9 @@ def ziskej_id_obci():
     return id_list
 
 def ziskej_nazvy_stran():
-   
+    """Z první detailní stránky obce získá seznam kandidujících stran."""
     detailni_url = ziskej_detailni_url()
-    if len(detailni_url) == 0:
+    if not detailni_url:
         return []
     odpoved = requests.get(detailni_url[0])
     detail_stranka = BeautifulSoup(odpoved.text, "html.parser")
@@ -71,9 +72,14 @@ def ziskej_nazvy_stran():
     return strany
 
 def sesbir_data_volicu():
-  
-    url_list = ziskej_detailni_url()
-    for url in url_list:
+    """
+    Pro každou detailní stránku obce získá údaje:
+      - registrovaní voliči (headers="sa2"),
+      - vydané obálky (headers="sa3"),
+      - platné hlasy (headers="sa6")
+    a uloží je do globálních seznamů.
+    """
+    for url in ziskej_detailni_url():
         odpoved = requests.get(url)
         detail_stranka = BeautifulSoup(odpoved.text, "html.parser")
         bunky = detail_stranka.find_all("td", headers="sa2")
@@ -87,10 +93,12 @@ def sesbir_data_volicu():
             platne_hlasy.append(bunka.text.replace('\xa0', ' '))
 
 def ziskej_procenta_hlasu():
-   
+    """
+    Pro každou obec získá procentuální výsledky hlasů pro jednotlivé strany.
+    Vrací seznam, kde každý prvek je seznam procent pro jednu obec.
+    """
     vsechna_procenta = []
-    url_list = ziskej_detailni_url()
-    for url in url_list:
+    for url in ziskej_detailni_url():
         stranka = nacti_stranku(url)
         procenta = []
         bunky = stranka.find_all("td", class_="cislo", headers=["t1sb4", "t2sb4"])
@@ -100,7 +108,10 @@ def ziskej_procenta_hlasu():
     return vsechna_procenta
 
 def vytvor_radky_csv():
-   
+    """
+    Sestaví řádky pro CSV soubor: ID obce, název, registrovaní voliči,
+    vydané obálky, platné hlasy a procenta hlasů kandidujících stran.
+    """
     radky = []
     sesbir_data_volicu()
     nazvy_obci = ziskej_nazvy_obci()
@@ -113,7 +124,6 @@ def vytvor_radky_csv():
     return radky
 
 def main():
-   
     hlavicka = ['Kód obce', 'Název obce', 'Voliči v seznamu', 'Vydané obálky', 'Platné hlasy']
     radky_csv = vytvor_radky_csv()
     strany = ziskej_nazvy_stran()
